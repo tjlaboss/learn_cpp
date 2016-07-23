ba6e89302222f1c6c8f5fcc9c0fa825a92c5bdc4
tjlaboss <travis@ktc-technology.com>
2016-07-23 07:38:46 -0400

    Learned how to get user input with cin.

---
+++ 03_User_Input/input.cpp
@@ -0,0 +1,16 @@
+// Input
+// Getting user input in C++ for the first time
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x;
+	cout << "Give me a number: ";
+	cin >> x;
+	cout << "\nYour number as an integer is " << x << '\n';
+	// cout << "Do stuff here\n";
+	return 0;
+}
+
