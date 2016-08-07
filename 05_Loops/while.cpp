6a98ec5501bb94e9536f4cd18991e0f4e8c311ee
tjlaboss <travis@ktc-technology.com>
2016-08-06 23:55:12 -0400

    Learned 'while' and 'do while'.

--- 05_Loops/while.cpp
+++ 05_Loops/while.cpp
@@ -5,7 +5,26 @@ using namespace std;
 
 int main()
 {
-	// cout << "Do stuff here\n";
+	int x = 0;
+	while ( x <= 5 )
+		{
+		// no curly braces needed
+		// for only one line
+		x = x + 1;
+		cout << "x = " << x << '\n';
+		}
+
+	cout << "------\n";
+
+	int y = 5;
+	do
+		{
+		cout << "y = " << y << '\n';
+		y = y - 1;
+		}
+	while (y > 0);
+
+
 	return 0;
 }
 
